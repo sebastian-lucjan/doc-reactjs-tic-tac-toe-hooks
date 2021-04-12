93cfe64628df0320e60a40cf96d80eca74bc7a0e
sebastian-lucjan <sebastian.lucjan@gmail.com>
2021-04-12 11:58:27 +0200

    Fork base code from class doc project

---
+++ src/components/Square.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Square = (props) => {
+  return (
+    <button onClick={props.onClick} className="square">
+      {props.value}
+    </button>
+  );
+};
+// this.props.fieldNumber
