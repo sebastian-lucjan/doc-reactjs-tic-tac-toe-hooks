39e9b885bd7dbb52e6f1ec5be7ae2fa52c9e65e2
sebastian-lucjan <sebastian.lucjan@gmail.com>
2021-04-12 15:49:19 +0200

    Styles start by add node scss, add _Square.module.scss

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import styles from '../components/_Square.module.scss';
 
 export const Square = (props) => {
   return (
-    <button onClick={props.onClick} className="square">
+    <button onClick={props.onClick} className={styles.square}>
       {props.value}
     </button>
   );
